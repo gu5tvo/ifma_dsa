cd961981910b787cfab074439d98afdf811b93a3
gu5tvo <gustavpassinho@gmail.com>
2023-05-23 14:55:58 -0300

    add: circular dinamic allocation

--- circular.c
+++ circular.c
@@ -1,71 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct list {
   int data; 
-  struct list * next;
+  struct list *next;
 };
 
-typedef struct list List ;
+typedef struct list List;
 
-List * create_lst() {
-  return NULL; //void list
+List *create_lst() {
+  return NULL; // void list
 }
 
-int void_lst(List * l) {
-  return (l == NULL); // 1 if the list is not void, 0 if it's void
+int is_void_lst(List *l) {
+  return (l == NULL); // 1 if the list is void, 0 if it's not void
 }
 
-List * insert_lst (List ** l, int data) {
-  List * new = (List*) malloc(sizeof(List));
-  new->data = data;
-  new->next = *l;
-  return new;
-}
-
-void print_lst(List ** l){
-  List * p;
-  for(p = *l; p!=NULL; p = p->next){
-    printf("info = %d", p->data);
-  }
-}
-
-List * search_lst(List ** l, int target){
-  List * p;
-  for(p = *l; p!=NULL; p = p->next){
-    if(p->data == target)
-      return p; //target found
-  }
-  return NULL; //target not found
-}
-
-List * remove_lst(List ** l, int target){
-  List * prev = NULL;
-  List * p;
-  if(!search_lst(l, target))
-    return *l; //target not found, return the original list 
+List *insert_lst(List **l, int data) {
+  List *new_node = (List*) malloc(sizeof(List));
+  new_node->data = data;
+  new_node->next = *l;
   
-  if(prev == NULL){
-    *l = p->next;
-  }else{
-    prev->next = p->next;
+  if (*l == NULL) {
+    new_node->next = new_node; // make the new node point to itself
+  } else {
+    List *last = *l;
+    while (last->next != *l) {
+      last = last->next;
+    }
+    last->next = new_node; // make the last node point to the new node
   }
-  free(p);
+  
+  *l = new_node; // update the head of the list
+  return new_node;
+}
+
+void print_lst(List *l) {
+  List *p = l;
+  if (l != NULL) {
+    do {
+      printf("info = %d\n", p->data);
+      p = p->next;
+    } while (p != l);
+  }
+}
+
+List *search_lst(List *l, int target) {
+  List *p = l;
+  if (l != NULL) {
+    do {
+      if (p->data == target)
+        return p; // target found
+      p = p->next;
+    } while (p != l);
+  }
+  return NULL; // target not found
+}
+
+List *remove_lst(List **l, int target) {
+  List *prev = NULL;
+  List *p = *l;
+  if (!search_lst(*l, target))
+    return *l; // target not found, return the original list
+  
+  do {
+    if (p->data == target) {
+      if (prev == NULL) {
+        *l = p->next;
+      } else {
+        prev->next = p->next;
+      }
+      
+      if (p == *l) {
+        *l = NULL; // the list became empty
+      }
+      
+      free(p);
+      break;
+    }
+    prev = p;
+    p = p->next;
+  } while (p != *l);
+  
   return *l;
 }
 
-void distroy_lst(List ** l){
-  List * p = *l;
-  while (p!=NULL){
-    List * t = p->next; //store the referece of next
+void destroy_lst(List **l) {
+  List *p = *l;
+  while (p != NULL) {
+    List *t = p->next; // store the reference of the next node
+    if (t == *l) {
+      free(p);
+      break; // avoid an infinite loop for circular lists
+    }
     free(p);
     p = t;
   }
-
+  *l = NULL; // update the head of the list
 }
 
-int main () {
-  List * l = create_lst();
+int main() {
+  List *l = create_lst();
   insert_lst(&l, 23);
+  insert_lst(&l, 42);
+  insert_lst(&l, 56);
+  insert_lst(&l, 78);
+  
+  // Print the circular list
+  printf("Circular List:\n");
+  print_lst(l);
+  
+  // Example usage of other functions
+  List *found = search_lst(l, 42);
+  if (found != NULL) {
+    printf("Target found: %d\n", found->data);
+  } else {
+    printf("Target not found.\n");
+
+  remove_lst(&l, 56);
+
+  // Print the updated circular list
+  printf("Updated Circular List:\n");
+  print_lst(l);
+
+  // Destroy the circular list
+  destroy_lst(&l);
+
+  return 0;
+  }
 }
+
